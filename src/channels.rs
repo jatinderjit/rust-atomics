23a84f8f283303f0ca6cc22e8b81434082db1826
Jatinderjit Singh <jatinderjit89@gmail.com>
2024-01-21 00:17:47 +0530

    Generic channel

---
+++ src/channels.rs
@@ -0,0 +1,33 @@
+use std::{
+    collections::VecDeque,
+    sync::{Condvar, Mutex},
+};
+
+pub struct Channel<T> {
+    queue: Mutex<VecDeque<T>>,
+    ready: Condvar,
+}
+
+impl<T> Channel<T> {
+    pub fn new() -> Self {
+        Self {
+            queue: Mutex::new(VecDeque::new()),
+            ready: Condvar::new(),
+        }
+    }
+
+    pub fn send(&self, message: T) {
+        self.queue.lock().unwrap().push_back(message);
+        self.ready.notify_one();
+    }
+
+    pub fn wait(&self) -> T {
+        let mut q = self.queue.lock().unwrap();
+        loop {
+            if let Some(message) = q.pop_front() {
+                return message;
+            }
+            q = self.ready.wait(q).unwrap();
+        }
+    }
+}
