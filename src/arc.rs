4762d6e014d5b1bc5512a1ebfa9c1d7edd94cd29
Jatinderjit Singh <jatinderjit89@gmail.com>
2024-01-25 01:20:59 +0530

    Add `Arc::get_mut` for conditional mut ref to underlying data

--- src/arc.rs
+++ src/arc.rs
@@ -1,12 +1,13 @@
 use std::{
     ops::Deref,
     ptr::NonNull,
-    sync::atomic::{AtomicUsize, Ordering::Relaxed},
+    sync::atomic::{fence, AtomicUsize, Ordering::*},
 };
 
 struct Inner<T: ?Sized> {
+    /// Total number of `Arc` instances.
     count: AtomicUsize,
-    value: T,
+    data: T,
 }
 
 /// Arc (Atomically Reference Counted) is a thread-safe version of `Rc`.
@@ -17,9 +18,9 @@ pub struct Arc<T: ?Sized> {
 }
 
 impl<T> Arc<T> {
-    pub fn new(value: T) -> Self {
+    pub fn new(data: T) -> Self {
         let inner = Inner {
-            value,
+            data,
             count: AtomicUsize::new(1),
         };
         let inner = Box::into_raw(Box::new(inner));
@@ -35,6 +36,21 @@ impl<T> Arc<T> {
     pub fn strong_count(&self) -> usize {
         self.inner().count.load(Relaxed)
     }
+
+    /// Get a mutable reference to the underlying data, only if this is the only
+    /// reference to it.
+    // The function doesn't take `&mut self` as an argument so that it can only
+    // be called as `Arc::get_mut(&mut a)`. This is advisable for types that
+    // implement `Deref`, to avoid ambiguity with a similarly named method on
+    // the underlying `T`.
+    pub fn get_mut(arc: &mut Self) -> Option<&mut T> {
+        if arc.inner().count.load(Relaxed) == 1 {
+            fence(Acquire);
+            unsafe { Some(&mut arc.inner.as_mut().data) }
+        } else {
+            None
+        }
+    }
 }
 
 /// `Arc<T>` can be passed between threads, if `T` can be. Since `Arc<T>` also
@@ -48,7 +64,7 @@ unsafe impl<T: Send + Sync> Sync for Arc<T> {}
 impl<T> Deref for Arc<T> {
     type Target = T;
     fn deref(&self) -> &Self::Target {
-        &self.inner().value
+        &self.inner().data
     }
 }
 
@@ -61,8 +77,27 @@ impl<T> Clone for Arc<T> {
 
 impl<T: ?Sized> Drop for Arc<T> {
     fn drop(&mut self) {
-        let prev_count = unsafe { self.inner.as_mut() }.count.fetch_sub(1, Relaxed);
+        // This needs to be synchronized only when the Inner struct is getting
+        // dropped.
+        let prev_count = unsafe { self.inner.as_mut() }.count.fetch_sub(1, Release);
         if prev_count == 1 {
+            // From the official `std::sync::Arc` docs:
+            //
+            // This fence is needed to prevent reordering of use of data and the
+            // deletion of data. This `Acquire` synchronizes with the `Acquire`
+            // of the drop.
+            //
+            // As explained in the [Boost documentation][1],
+            //
+            // > It is important to enforce any possible access to the object in one
+            // > thread (through an existing reference) to *happen before* deleting
+            // > the object in a different thread. This is achieved by a "release"
+            // > operation after dropping a reference (any access to the object
+            // > through this reference must obviously happened before), and an
+            // > "acquire" operation before deleting the object.
+            //
+            // [1]: (www.boost.org/doc/libs/1_55_0/doc/html/atomic/usage_examples.html)
+            fence(Acquire);
             unsafe { drop(Box::from_raw(self.inner.as_mut())) };
         }
     }
@@ -76,41 +111,50 @@ mod test {
 
     #[test]
     fn single_thread() {
-        let data = Arc::new(123);
-        assert_eq!(Arc::strong_count(&data), 1);
+        let arc = Arc::new(123);
+        assert_eq!(Arc::strong_count(&arc), 1);
 
-        let data2 = Arc::clone(&data);
-        assert_eq!(Arc::strong_count(&data), 2);
+        let arc2 = Arc::clone(&arc);
+        assert_eq!(Arc::strong_count(&arc), 2);
 
-        drop(data);
-        assert_eq!(Arc::strong_count(&data2), 1);
+        drop(arc);
+        assert_eq!(Arc::strong_count(&arc2), 1);
 
-        assert_eq!(*data2, 123);
+        assert_eq!(*arc2, 123);
     }
 
     #[test]
     fn multiple_threads() {
-        let data = Arc::new(123);
+        let arc = Arc::new(123);
         thread::scope(|s| {
-            let data2 = Arc::clone(&data);
+            let data2 = Arc::clone(&arc);
             s.spawn(move || {
                 assert_eq!(*data2, 123);
                 assert_eq!(Arc::strong_count(&data2), 2);
             });
-            s.spawn(|| assert_eq!(*data, 123));
+            s.spawn(|| assert_eq!(*arc, 123));
         });
-        assert_eq!(Arc::strong_count(&data), 1);
+        assert_eq!(Arc::strong_count(&arc), 1);
     }
 
     #[test]
     fn sync() {
-        let data = Arc::new(Mutex::new(1));
+        let arc = Arc::new(Mutex::new(1));
         thread::scope(|s| {
             s.spawn(|| {
-                let mut guard = data.lock().unwrap();
+                let mut guard = arc.lock().unwrap();
                 *guard = 2;
             });
         });
-        assert_eq!(*data.lock().unwrap(), 2);
+        assert_eq!(*arc.lock().unwrap(), 2);
+    }
+
+    #[test]
+    fn mut_ref() {
+        let mut arc = Arc::new(1);
+        let arc2 = Arc::clone(&arc);
+        assert!(Arc::get_mut(&mut arc).is_none());
+        drop(arc2);
+        assert!(Arc::get_mut(&mut arc).is_some());
     }
 }
