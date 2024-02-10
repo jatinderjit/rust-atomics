037d62c1216f6de3e107e15ba0a4f2df93cc5729
Jatinderjit Singh <jatinderjit89@gmail.com>
2024-02-11 05:28:09 +0530

    Add Mutex benchmarking code

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,29 @@
-use atomics::{once_data, processor, spinlock};
+use std::{thread, time::Instant};
+
+use atomics::{mutex::Mutex, once_data, processor, spinlock};
 
 fn main() {
-    once_data::check::run();
-    spinlock::check::run();
-    processor::run();
+    if false {
+        once_data::check::run();
+        spinlock::check::run();
+        processor::run();
+    }
+    bench_mutex();
+}
+
+fn bench_mutex() {
+    let m = Mutex::new(0);
+    std::hint::black_box(&m);
+    let start = Instant::now();
+    thread::scope(|s| {
+        for _ in 0..10 {
+            s.spawn(|| {
+                for _ in 0..1_000_000 {
+                    *m.lock() += 1;
+                }
+            });
+        }
+    });
+    let duration = start.elapsed();
+    println!("locked {} times in {:?}", *m.lock(), duration);
 }
