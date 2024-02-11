bf07ab61346dc559c26a7e9f12670e790cc2d896
Jatinderjit Singh <jatinderjit89@gmail.com>
2024-02-11 20:20:08 +0530

    Fix Condvar logic

--- src/condvar.rs
+++ src/condvar.rs
@@ -1,35 +1,44 @@
 use std::sync::atomic::{AtomicU32, Ordering::*};
 
-use atomic_wait::{wait, wake_one};
+use atomic_wait::{wait, wake_all, wake_one};
 
 use crate::mutex::MutexGuard;
 
-const NOT_READY: u32 = 0;
-const READY: u32 = 1;
-
 pub struct Condvar {
-    state: AtomicU32,
+    counter: AtomicU32,
 }
 
 impl Condvar {
     pub const fn new() -> Self {
         Self {
-            state: AtomicU32::new(NOT_READY),
+            counter: AtomicU32::new(0),
         }
     }
 
+    /// Wait allows spurious wake-ups
     pub fn wait<'a, T>(&self, guard: MutexGuard<'a, T>) -> MutexGuard<'a, T> {
+        // Load the value before unlocking for an "atomic" behaviour.
+        // This prevents losing a signal between unlocking the mutex and reading
+        // the value (if we read this after unlocking).
+        let value = self.counter.load(Relaxed);
+
         let mutex = guard.mutex;
         drop(guard);
-        while self.state.swap(NOT_READY, Relaxed) != READY {
-            wait(&self.state, NOT_READY);
-        }
+
+        // Wait only if the counter hasn't changed since unlocking.
+        wait(&self.counter, value);
+
         mutex.lock()
     }
 
     pub fn notify_one(&self) {
-        self.state.store(READY, Relaxed);
-        wake_one(&self.state);
+        self.counter.fetch_add(1, Relaxed);
+        wake_one(&self.counter);
+    }
+
+    pub fn notify_all(&self) {
+        self.counter.fetch_add(1, Relaxed);
+        wake_all(&self.counter);
     }
 }
 
@@ -47,6 +56,7 @@ mod test {
         thread::scope(|s| {
             s.spawn(|| {
                 let guard = val.lock();
+                // Flaky, since we don't handle spurious wake-ups.
                 let guard = non_zero.wait(guard);
                 assert_eq!(*guard, 1);
             });
@@ -57,17 +67,32 @@ mod test {
     }
 
     #[test]
-    fn early_notify() {
-        let val = Mutex::new(0);
-        let non_zero = Condvar::new();
+    fn test_allow_spurious() {
+        // Copied test case
+        // https://github.com/m-ou-se/rust-atomics-and-locks/blob/main/src/ch9_locks/condvar_1.rs
+        let mutex = Mutex::new(0);
+        let condvar = Condvar::new();
+
+        let mut wakeups = 0;
+
         thread::scope(|s| {
-            *val.lock() = 1;
-            non_zero.notify_one();
             s.spawn(|| {
-                let guard = val.lock();
-                let guard = non_zero.wait(guard);
-                assert_eq!(*guard, 1);
+                thread::sleep(Duration::from_secs(1));
+                *mutex.lock() = 123;
+                condvar.notify_one();
             });
+
+            let mut m = mutex.lock();
+            while *m < 100 {
+                m = condvar.wait(m);
+                wakeups += 1;
+            }
+
+            assert_eq!(*m, 123);
         });
+
+        // Check that the main thread actually did wait (not busy-loop),
+        // while still allowing for a few spurious wake ups.
+        assert!(wakeups < 10);
     }
 }
